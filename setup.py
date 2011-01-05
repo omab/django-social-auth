0b9ad8c835747224a2cfa536e26347e9f8a7ff2e
Matías Aguirre <matiasaguirre@gmail.com>
2011-01-05 11:56:13 -0200

    Updated setup.py file to install properly, added version to social_auth module

--- setup.py
+++ setup.py
@@ -4,24 +4,45 @@ from os.path import join, dirname
 from setuptools import setup
 
 
+version = __import__('social_auth').__version__
+
+LONG_DESCRIPTION = """
+Django Social Auth is an easy to setup social authentication/registration
+mechanism for Django projects.
+
+Crafted using base code from django-twitter-oauth_ and django-openid-auth_,
+implements a common interface to define new authentication providers from
+third parties.
+"""
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
 setup(name='django-social-auth',
-      version='0.1',
+      version=version,
       author='Matías Aguirre',
       author_email='matiasaguirre@gmail.com',
       description='Django social authentication made simple.',
       license='GPL',
       keywords='django, openid, oath, social auth, application',
       url='https://github.com/omab/django-social-auth',
-      download_url='git+git://github.com/omab/django-social-auth.git#egg=django-social-auth',
       packages=['social_auth'],
-      long_description=open(join(dirname(__file__), 'README.rst')).read(),
-      requires=['django (>=1.2)',
-                'oauth (>=1.0)',
-                'python_openid (>=2.2)'],
+      long_description=long_description(),
+      install_requires=['django>=1.2',
+                        'oauth>=1.0',
+                        'python_openid>=2.2'],
       classifiers=['Framework :: Django',
                    'Development Status :: 4 - Beta',
                    'Topic :: Internet',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
                    'Intended Audience :: Developers',
                    'Environment :: Web Environment',
-                   'Programming Language :: Python'])
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7'])
--- social_auth/__init__.py
+++ social_auth/__init__.py
@@ -2,3 +2,5 @@
 Django-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
+version = (0, 1, 0)
+__version__ = '.'.join(map(str, version))
