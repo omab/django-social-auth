0d2bf0c00efce688f171ae43ac6f5e81f9496ced
Matías Aguirre <matiasaguirre@gmail.com>
2011-05-18 14:42:58 -0300

    Small changes to selenium tests

--- contrib/tests/test_core.py
+++ contrib/tests/test_core.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import urlparse
+
 from selenium import webdriver
 
 from django.test import TestCase
@@ -12,6 +14,9 @@ class BackendsTest(TestCase):
     def tearDown(self):
         self.driver.quit()
 
+    def url(self, path):
+        return urlparse.urljoin(settings.TEST_DOMAIN, path)
+
     def test_twitter_backend(self):
         # We grab the Twitter testing user details from settings file
         TEST_TWITTER_USER = getattr(settings, 'TEST_TWITTER_USER', None)
@@ -19,26 +24,26 @@ class BackendsTest(TestCase):
         self.assertTrue(TEST_TWITTER_USER)
         self.assertTrue(TEST_TWITTER_PASSWORD)
 
-        self.driver.get("http://social.matiasaguirre.net/login/twitter/")
+        self.driver.get(self.url('/login/twitter/'))
 
         # We log in
-        username_field = self.driver.find_element_by_id("username_or_email")
+        username_field = self.driver.find_element_by_id('username_or_email')
         username_field.send_keys(TEST_TWITTER_USER)
 
-        password_field = self.driver.find_element_by_id("session[password]")
+        password_field = self.driver.find_element_by_id('session[password]')
         password_field.send_keys(TEST_TWITTER_PASSWORD)
         password_field.submit()
 
         # The application might be already allowed
         try:
-            self.driver.find_element_by_id("allow").click()
+            self.driver.find_element_by_id('allow').click()
         except:
             pass
 
         # We check the user logged in
-        heading = self.driver.find_element_by_id("heading")
+        heading = self.driver.find_element_by_id('heading')
         if not heading.text == u'Logged in!':
-            raise Exception("The user didn't logged in")
+            raise Exception('The user didn\'t logged in')
 
         # Here we could test the User's fields
 
@@ -48,26 +53,26 @@ class BackendsTest(TestCase):
         self.assertTrue(TEST_GOOGLE_USER)
         self.assertTrue(TEST_GOOGLE_PASSWORD)
 
-        self.driver.get("http://social.matiasaguirre.net/login/google-oauth/")
+        self.driver.get(self.url('/login/google-oauth/'))
 
         # We log in
-        username_field = self.driver.find_element_by_id("Email")
+        username_field = self.driver.find_element_by_id('Email')
         username_field.send_keys(TEST_GOOGLE_USER)
 
-        password_field = self.driver.find_element_by_id("Passwd")
+        password_field = self.driver.find_element_by_id('Passwd')
         password_field.send_keys(TEST_GOOGLE_PASSWORD)
         password_field.submit()
 
         # The application might be already allowed
         try:
-            self.driver.find_element_by_id("allow").click()
+            self.driver.find_element_by_id('allow').click()
         except:
             pass
 
         # We check the user logged in
-        heading = self.driver.find_element_by_id("heading")
+        heading = self.driver.find_element_by_id('heading')
         if not heading.text == u'Logged in!':
-            raise Exception("The user didn't logged in")
+            raise Exception('The user didn\'t logged in')
 
         # Here we could test the User's fields
 
@@ -77,26 +82,26 @@ class BackendsTest(TestCase):
         self.assertTrue(TEST_GOOGLE_USER)
         self.assertTrue(TEST_GOOGLE_PASSWORD)
 
-        self.driver.get("http://social.matiasaguirre.net/login/google-oauth2/")
+        self.driver.get(self.url('/login/google-oauth2/'))
 
         # We log in
-        username_field = self.driver.find_element_by_id("Email")
+        username_field = self.driver.find_element_by_id('Email')
         username_field.send_keys(TEST_GOOGLE_USER)
 
-        password_field = self.driver.find_element_by_id("Passwd")
+        password_field = self.driver.find_element_by_id('Passwd')
         password_field.send_keys(TEST_GOOGLE_PASSWORD)
         password_field.submit()
 
         # The application might be already allowed
         try:
-            self.driver.find_element_by_id("submit_approve_access").click()
+            self.driver.find_element_by_id('submit_approve_access').click()
         except:
             pass
 
         # We check the user logged in
-        heading = self.driver.find_element_by_id("heading")
+        heading = self.driver.find_element_by_id('heading')
         if not heading.text == u'Logged in!':
-            raise Exception("The user didn't logged in")
+            raise Exception('The user didn\'t logged in')
 
         # Here we could test the User's fields
 
@@ -106,20 +111,20 @@ class BackendsTest(TestCase):
         self.assertTrue(TEST_FACEBOOK_USER)
         self.assertTrue(TEST_FACEBOOK_PASSWORD)
 
-        self.driver.get("http://social.matiasaguirre.net/login/facebook/")
+        self.driver.get(self.url('/login/facebook/'))
 
         # We log in
-        username_field = self.driver.find_element_by_id("email")
+        username_field = self.driver.find_element_by_id('email')
         username_field.send_keys(TEST_FACEBOOK_USER)
 
-        password_field = self.driver.find_element_by_id("pass")
+        password_field = self.driver.find_element_by_id('pass')
         password_field.send_keys(TEST_FACEBOOK_PASSWORD)
         password_field.submit()
 
         # We check the user logged in
-        heading = self.driver.find_element_by_id("heading")
+        heading = self.driver.find_element_by_id('heading')
         if not heading.text == u'Logged in!':
-            raise Exception("The user didn't logged in")
+            raise Exception('The user didn\'t logged in')
 
         # Here we could test the User's fields
 
@@ -129,19 +134,19 @@ class BackendsTest(TestCase):
         self.assertTrue(TEST_LINKEDIN_USER)
         self.assertTrue(TEST_LINKEDIN_PASSWORD)
 
-        self.driver.get("http://social.matiasaguirre.net/login/linkedin/")
+        self.driver.get(self.url('/login/linkedin/'))
 
         # We log in
-        username_field = self.driver.find_element_by_id("session_key-oauthAuthorizeForm")
+        username_field = self.driver.find_element_by_id('session_key-oauthAuthorizeForm')
         username_field.send_keys(TEST_LINKEDIN_USER)
 
-        password_field = self.driver.find_element_by_id("session_password-oauthAuthorizeForm")
+        password_field = self.driver.find_element_by_id('session_password-oauthAuthorizeForm')
         password_field.send_keys(TEST_LINKEDIN_PASSWORD)
         password_field.submit()
 
         # We check the user logged in
-        heading = self.driver.find_element_by_id("heading")
+        heading = self.driver.find_element_by_id('heading')
         if not heading.text == u'Logged in!':
-            raise Exception("The user didn't logged in")
+            raise Exception('The user didn\'t logged in')
 
         # Here we could test the User's fields
