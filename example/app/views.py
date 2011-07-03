65ef1bb243b559b1a6f05a882aec3fa89be25ed6
Matías Aguirre <matiasaguirre@gmail.com>
2011-07-03 17:43:53 -0300

    Dynamic backend list on example app

--- example/app/views.py
+++ example/app/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from social_auth import __version__ as version
+from social_auth.backends import BACKENDS, OpenIdAuth, BaseOAuth, BaseOAuth2
 
 
 def home(request):
@@ -13,7 +14,9 @@ def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('done')
     else:
-        return render_to_response('home.html', {'version': version},
+        backends = grouped_backends()
+        return render_to_response('home.html', {'version': version,
+                                                'backends': backends},
                                   RequestContext(request))
 
 @login_required
@@ -21,7 +24,8 @@ def done(request):
     """Login complete view, displays user data"""
     ctx = {'accounts': request.user.social_auth.all(),
            'version': version,
-           'last_login': request.session.get('social_auth_last_login_backend')}
+           'last_login': request.session.get('social_auth_last_login_backend'),
+           'backends': grouped_backends()}
     return render_to_response('done.html', ctx, RequestContext(request))
 
 def error(request):
@@ -35,3 +39,22 @@ def logout(request):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/')
+
+
+def grouped_backends():
+    """Group backends by type"""
+    backends = {'oauth': [],
+                'oauth2': [],
+                'openid': []}
+
+    for name, backend in BACKENDS.iteritems():
+        if issubclass(backend, BaseOAuth2):
+            key = 'oauth2'
+        elif issubclass(backend, BaseOAuth):
+            key = 'oauth'
+        elif issubclass(backend, OpenIdAuth):
+            key = 'openid'
+        else:
+            print name, backend
+        backends[key].append((name, backend))
+    return backends
