3c4e91829c95d44e4dca0841c5341fe4c29a7d88
Matías Aguirre <matiasaguirre@gmail.com>
2011-08-14 06:03:22 -0300

    Make use of context processors on example app. Refs gh-113

--- example/app/views.py
+++ example/app/views.py
@@ -6,7 +6,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from social_auth import __version__ as version
-from social_auth.backends import BACKENDS, OpenIdAuth, BaseOAuth, BaseOAuth2
 
 
 def home(request):
@@ -14,18 +13,14 @@ def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('done')
     else:
-        backends = grouped_backends()
-        return render_to_response('home.html', {'version': version,
-                                                'backends': backends},
+        return render_to_response('home.html', {'version': version},
                                   RequestContext(request))
 
 @login_required
 def done(request):
     """Login complete view, displays user data"""
-    ctx = {'accounts': request.user.social_auth.all(),
-           'version': version,
-           'last_login': request.session.get('social_auth_last_login_backend'),
-           'backends': grouped_backends()}
+    ctx = {'version': version,
+           'last_login': request.session.get('social_auth_last_login_backend')}
     return render_to_response('done.html', ctx, RequestContext(request))
 
 def error(request):
@@ -39,22 +34,3 @@ def logout(request):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/')
-
-
-def grouped_backends():
-    """Group backends by type"""
-    backends = {'oauth': [],
-                'oauth2': [],
-                'openid': []}
-
-    for name, backend in BACKENDS.iteritems():
-        if issubclass(backend, BaseOAuth2):
-            key = 'oauth2'
-        elif issubclass(backend, BaseOAuth):
-            key = 'oauth'
-        elif issubclass(backend, OpenIdAuth):
-            key = 'openid'
-        else:
-            print name, backend
-        backends[key].append((name, backend))
-    return backends
