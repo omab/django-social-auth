5bb659026bffba8bf5edf113a4049beece4d0b12
Matías Aguirre <matiasaguirre@gmail.com>
2011-09-16 17:56:56 -0300

    Send signal for cases where tracking failed registering is needed

--- social_auth/backends/__init__.py
+++ social_auth/backends/__init__.py
@@ -33,7 +33,8 @@ from django.db.utils import IntegrityError
 
 from social_auth.models import UserSocialAuth
 from social_auth.store import DjangoOpenIDStore
-from social_auth.signals import pre_update, socialauth_registered
+from social_auth.signals import pre_update, socialauth_registered, \
+                                socialauth_not_registered
 
 
 # OpenID configuration
@@ -111,6 +112,10 @@ class SocialAuthBackend(ModelBackend):
         except UserSocialAuth.DoesNotExist:
             if user is None:  # new user
                 if not CREATE_USERS or not kwargs.get('create_user', True):
+                    # Send signal for cases where tracking failed registering
+                    # is useful.
+                    socialauth_not_registered.send(uid=uid, response=response,
+                                                   details=details)
                     return None
 
                 email = details.get('email')
