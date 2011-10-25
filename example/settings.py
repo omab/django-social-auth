cad21674769580acb3542d19f8458836a62562eb
Matías Aguirre <matiasaguirre@gmail.com>
2011-10-25 14:56:59 -0200

    Fix Flickr attribute getter

--- example/settings.py
+++ example/settings.py
@@ -69,6 +69,7 @@ AUTHENTICATION_BACKENDS = (
     'social_auth.backends.google.GoogleBackend',
     'social_auth.backends.yahoo.YahooBackend',
     'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.contrib.flickr.FlickrBackend',
     'social_auth.backends.OpenIDBackend',
     'social_auth.backends.contrib.livejournal.LiveJournalBackend',
     'django.contrib.auth.backends.ModelBackend',
--- social_auth/backends/contrib/flickr.py
+++ social_auth/backends/contrib/flickr.py
@@ -39,7 +39,6 @@ class FlickrBackend(OAuthBackend):
 
     def get_user_details(self, response):
         """Return user details from Flickr account"""
-        print response
         return {USERNAME: response.get('id'),
                 'email': '',
                 'first_name': response.get('fullname')}
@@ -64,9 +63,13 @@ class FlickrAuth(ConsumerBasedOAuth):
         response = self.fetch_response(request)
         token = Token.from_string(response)
         params = parse_qs(response)
-        token.user_nsid = params['user_nsid'][0]
-        token.fullname = params['fullname'][0]
-        token.username = params['username'][0]
+
+        token.user_nsid = params['user_nsid'][0] if 'user_nsid' in params \
+                                                 else None
+        token.fullname = params['fullname'][0] if 'fullname' in params \
+                                               else None
+        token.username = params['username'][0] if 'username' in params \
+                                               else None
         return token
 
     def user_data(self, access_token):
