2a2c9e01b51b10594da97c5ce273073931e44c43
Johannes Holmberg <johannes@update.uu.se>
2012-02-16 15:24:07 +0100

    Revert "Add own tuple_index function to stay compatible with python 2.5"
    This reverts commit 7e6af723e15a785b403010de4b44b49fce924e91.

--- social_auth/backends/pipeline/misc.py
+++ social_auth/backends/pipeline/misc.py
@@ -4,22 +4,18 @@ from social_auth.utils import setting
 
 PIPELINE_ENTRY = 'social_auth.backends.pipeline.misc.save_status_to_session'
 
-def tuple_index(t, e):
-    for (i, te) in enumerate(t):
-        if te == e:
-            return i
-    return None
 
 def save_status_to_session(request, auth, *args, **kwargs):
     """Saves current social-auth status to session."""
     next_entry = setting('SOCIAL_AUTH_PIPELINE_RESUME_ENTRY')
 
-    if next_entry:
-        idx = tuple_index(PIPELINE, next_entry)
-    else:
-        idx = tuple_index(PIPELINE, PIPELINE_ENTRY)
-        if idx:
-            idx += 1
+    try:
+        if next_entry:
+            idx = PIPELINE.index(next_entry)
+        else:
+            idx = PIPELINE.index(PIPELINE_ENTRY) + 1
+    except ValueError:
+        idx = None
 
     data = auth.to_session_dict(idx, *args, **kwargs)
 
