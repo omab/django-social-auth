8b35632244d8e4594c3e04bc099ba86c6668e21c
Matías Aguirre <matiasaguirre@gmail.com>
2012-07-14 16:54:01 -0300

    Only import classes from db backends. Closes #397

--- social_auth/models.py
+++ social_auth/models.py
@@ -1,4 +1,6 @@
 """Social auth models"""
+import types
+
 from django.utils.importlib import import_module
 
 from social_auth.utils import setting
@@ -7,5 +9,7 @@ from social_auth.utils import setting
 SOCIAL_AUTH_MODELS_MODULE = import_module(setting('SOCIAL_AUTH_MODELS',
                                                'social_auth.db.django_models'))
 
-globals().update((name, getattr(SOCIAL_AUTH_MODELS_MODULE, name))
-                    for name in dir(SOCIAL_AUTH_MODELS_MODULE))
+globals().update((name, value) for name, value in
+                    ((name, getattr(SOCIAL_AUTH_MODELS_MODULE, name))
+                        for name in dir(SOCIAL_AUTH_MODELS_MODULE))
+                    if isinstance(value, (type, types.ClassType)))
