d41b5a5a5f06c50a3db0f8204bbb2e73a3798e0b
Steven Cummings <estebistec@gmail.com>
2012-08-07 20:49:02 -0500

    Much simpler SocialAuthExceptionMiddleware

--- social_auth/middleware.py
+++ social_auth/middleware.py
@@ -3,48 +3,35 @@
 
 from django.conf import settings
 from django.contrib import messages
-from django.core.urlresolvers import get_callable
 from django.shortcuts import redirect
-from django.utils.translation import ugettext_lazy as _
 
-from social_auth.backends.exceptions import AuthAlreadyAssociated
-
-
-AUTH_EXCEPTION_REDIRECT_URI = (getattr(settings,
-        'AUTH_EXCEPTION_REDIRECT_URI', None) or '/')
-
-
-def default_auth_exception_redirect_uri_func(request, exception):
-    return AUTH_EXCEPTION_REDIRECT_URI
-
-
-def get_auth_exception_redirect_uri_func():
-    AUTH_EXCEPTION_REDIRECT_URI_FUNC_NAME = getattr(settings,
-            'AUTH_EXCEPTION_REDIRECT_URI_FUNC', None)
-    if AUTH_EXCEPTION_REDIRECT_URI_FUNC_NAME:
-        func = get_callable(AUTH_EXCEPTION_REDIRECT_URI_FUNC_NAME)
-        if callable(func):
-            return func
-    return default_auth_exception_redirect_uri_func
-
-
-get_auth_exception_redirect_uri = get_auth_exception_redirect_uri_func()
+from social_auth.backends.exceptions import AuthException
 
 
 class SocialAuthExceptionMiddleware(object):
-    """Handle specific sub-types of social-auth's AuthException that are
-    not related to system or configuration errors, where the user can be
-    informed of the situations and, possibly, take corrective action.
+    """Middleware that handles Social Auth AuthExceptions by providing the user
+    with a message, logging an error, and redirecting to some next location.
 
-    Handling entails providing a message to the user via the Django
-    messages framework and then redirecting to either the URI specified
-    by the current social backend, or a configured default.
+    By default, the exception message itself is sent to the user and they are
+    redirected to the location specified in the LOGIN_ERROR_URL setting.
 
+    This middleware can be extended by overriding the get_message or
+    get_redirect_uri methods, which each accept request and exception.
     """
+
     def process_exception(self, request, exception):
-        if isinstance(exception, AuthAlreadyAssociated):
-            messages.error(request, _('That account is already in use '
-                'by another user'))
-            url = get_auth_exception_redirect_uri(request, exception)
+        if isinstance(exception, AuthException):
+            backend_name = exception.backend.AUTH_BACKEND.name
+            message = self.get_message(request, exception)
+            messages.error(request, message,
+                    extra_tags=u'social-auth {0}'.format(backend_name))
+
+            url = self.get_redirect_uri(request, exception)
             return redirect(url)
+
+    def get_message(self, request, exception):
+        return unicode(exception)
+
+    def get_redirect_uri(self, request, exception):
+        return settings.LOGIN_ERROR_URL
 
