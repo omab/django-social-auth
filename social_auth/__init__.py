e064aee038f1a1a3b52ac4df6af4d3097866b534
Matías Aguirre <matiasaguirre@gmail.com>
2012-11-11 10:39:19 -0200

    v0.7.9

--- social_auth/__init__.py
+++ social_auth/__init__.py
@@ -2,5 +2,5 @@
 Django-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 7, 8)
+version = (0, 7, 9)
 __version__ = '.'.join(map(str, version))
