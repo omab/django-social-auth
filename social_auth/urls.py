2db8c4874e10db7c78f4255df45b14a9484990ec
Troy <troytft@gmail.com>
2013-01-13 13:52:52 +0400

    Update social_auth/urls.py
    fix import error

--- social_auth/urls.py
+++ social_auth/urls.py
@@ -3,7 +3,7 @@ try:
     from django.conf.urls import patterns, url 
 except ImportError: 
     # for Django version less then 1.4
-    from django.conf.urls.default import patterns, url
+    from django.conf.urls.defaults import patterns, url
     
 from social_auth.views import auth, complete, disconnect
 
