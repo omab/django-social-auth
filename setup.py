b17205259ef47ec8bddc8b9513b51245ef4a3db6
Matías Aguirre <matiasaguirre@gmail.com>
2013-01-22 03:21:18 -0200

    Added zip_safe option to setup.py

--- setup.py
+++ setup.py
@@ -55,4 +55,5 @@ setup(name='django-social-auth',
                    'Environment :: Web Environment',
                    'Programming Language :: Python :: 2.5',
                    'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7'])
+                   'Programming Language :: Python :: 2.7'],
+      zip_safe=False)
