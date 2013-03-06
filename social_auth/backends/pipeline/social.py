1c69da6b799c5f1cfd6eccfc404df4ef41c415d7
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-06 16:05:43 -0200

    Flag new associations in the pipeline

--- social_auth/backends/pipeline/social.py
+++ social_auth/backends/pipeline/social.py
@@ -20,7 +20,9 @@ def social_auth_user(backend, uid, user=None, *args, **kwargs):
             })
         elif not user:
             user = social_user.user
-    return {'social_user': social_user, 'user': user}
+    return {'social_user': social_user,
+            'user': user,
+            'new_association': False}
 
 
 def associate_user(backend, user, uid, social_user=None, *args, **kwargs):
@@ -42,7 +44,9 @@ def associate_user(backend, user, uid, social_user=None, *args, **kwargs):
         return social_auth_user(backend, uid, user, social_user=social_user,
                                 *args, **kwargs)
     else:
-        return {'social_user': social, 'user': social.user}
+        return {'social_user': social,
+                'user': social.user,
+                'new_association': True}
 
 
 def load_extra_data(backend, details, response, uid, user, social_user=None,
