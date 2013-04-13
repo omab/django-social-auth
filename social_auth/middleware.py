7643d896a082073265baf0b3f8fb15b75466ef2b
Matías Aguirre <matiasaguirre@gmail.com>
2013-04-13 00:44:20 -0300

    Remove authenticated-user limitation to messages app usage. Refs #669

--- social_auth/middleware.py
+++ social_auth/middleware.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.shortcuts import redirect
 
 from social_auth.exceptions import SocialAuthBaseException
-from social_auth.utils import setting, backend_setting, get_backend_name
+from social_auth.utils import backend_setting, get_backend_name
 
 
 class SocialAuthExceptionMiddleware(object):
@@ -26,16 +26,13 @@ class SocialAuthExceptionMiddleware(object):
             backend_name = get_backend_name(self.backend)
             message = self.get_message(request, exception)
             url = self.get_redirect_uri(request, exception)
+            tags = ['social-auth']
+            if backend_name:
+                tags.append(backend_name)
 
-            if request.user.is_authenticated():
-                # Ensure that messages are added to authenticated users only,
-                # otherwise this fails
-                if backend_name:
-                    extra_tags = u'social-auth %s' % backend_name
-                else:
-                    extra_tags = ''
-                messages.error(request, message, extra_tags=extra_tags)
-            else:
+            try:
+                messages.error(request, message, extra_tags=' '.join(tags))
+            except messages.MessageFailure:  # messages app is not installed
                 url += ('?' in url and '&' or '?') + 'message=' + message
                 if backend_name:
                     url += '&backend=' + backend_name
@@ -50,7 +47,7 @@ class SocialAuthExceptionMiddleware(object):
     def raise_exception(self, request, exception):
         backend = self.backend
         return backend and \
-               backend_setting(backend, 'SOCIAL_AUTH_RAISE_EXCEPTIONS') 
+               backend_setting(backend, 'SOCIAL_AUTH_RAISE_EXCEPTIONS')
 
     def get_message(self, request, exception):
         return unicode(exception)
