d7a668eeff0b2b46121e9767e30a67e775c00c89
Matías Aguirre <matiasaguirre@gmail.com>
2013-07-13 23:52:20 -0300

    Build search_fields dynamically by checking field existence in user model. Closes #724

--- social_auth/admin.py
+++ social_auth/admin.py
@@ -6,11 +6,24 @@ if setting('SOCIAL_AUTH_MODELS') in (None, 'social_auth.db.django_models'):
     from django.contrib import admin
     from social_auth.models import UserSocialAuth, Nonce, Association
 
+    _User = UserSocialAuth.user_model()
+
+    if hasattr(_User, 'USERNAME_FIELD'):
+        username_field = _User.USERNAME_FIELD
+    elif hasattr(_User, 'username'):
+        username_field = 'username'
+    else:
+        username_field = None
+
+    fieldnames = ('first_name', 'last_name', 'email') + (username_field,)
+    all_names = _User._meta.get_all_field_names()
+    user_search_fields = ['user__' + name for name in fieldnames
+                                if name in all_names]
+
     class UserSocialAuthOption(admin.ModelAdmin):
         """Social Auth user options"""
         list_display = ('id', 'user', 'provider', 'uid')
-        search_fields = ('user__first_name', 'user__last_name', 'user__email',
-                'user__username')
+        search_fields = user_search_fields
         list_filter = ('provider',)
         raw_id_fields = ('user',)
         list_select_related = True
