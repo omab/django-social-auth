5b9487d668a113bdb063ae3cda6b47b332410589
Matías Aguirre <matiasaguirre@gmail.com>
2013-08-28 12:34:36 -0300

    Replace '-' with '_' on backend names to avoid duplicates. Closes #751

--- social_auth/context_processors.py
+++ social_auth/context_processors.py
@@ -43,7 +43,7 @@ def social_auth_by_name_backends(request):
     google-oauth2 becomes google_oauth2 when referenced in templates.
     """
     def context_value():
-        keys = get_backends().keys()
+        keys = [key.replace('-', '_') for key in get_backends().keys()]
         accounts = dict(zip(keys, [None] * len(keys)))
         user = request.user
         if hasattr(user, 'is_authenticated') and user.is_authenticated():
