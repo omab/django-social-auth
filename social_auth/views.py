9c4029c7107fcf8a8c4d5a2f787accfd7571c956
Matías Aguirre <matiasaguirre@gmail.com>
2013-08-29 18:01:14 -0300

    Remove utils module

--- social_auth/views.py
+++ social_auth/views.py
@@ -1,13 +1,22 @@
+from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
+from social.utils import setting_name
 from social.actions import do_auth, do_complete, do_disconnect
-from social.apps.django_app.utils import strategy
+from social.strategies.utils import get_strategy
+from social.apps.django_app.utils import strategy, BACKENDS, STORAGE
 from social.apps.django_app.views import _do_login
 
-from social_auth.utils import load_strategy
+
+STRATEGY = getattr(settings, setting_name('STRATEGY'),
+                   'social_auth.strategy.DSAStrategy')
+
+
+def load_strategy(*args, **kwargs):
+    return get_strategy(BACKENDS, STRATEGY, STORAGE, *args, **kwargs)
 
 
 @strategy('socialauth_complete', load_strategy=load_strategy)
